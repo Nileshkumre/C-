bfa6d4360266180fffb441ed9dabe9bba7f3a7d0
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-02-29 14:18:05 +0530

    9 - LoopingStatement

---
+++ 9_ LoopingStatements/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9__LoopingStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region While loop
+
+            //int start = 1; // instialization
+            //while(start <=10)// condition
+            //{
+            //    Console.WriteLine(start);
+            //    start++; // updation
+            //}
+
+            #endregion While loop
+
+            #region Do while loop
+
+            //int start = 1;
+            //do
+            //{
+            //    Console.WriteLine(start);
+            //    start++;
+            //} while (start <=5);
+
+
+            #endregion Do while loop
+
+            #region for loop 
+            //for (int name = 1; name <= 10; name++)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //for (int name = 1; name <= 10; name++)
+            //{
+            //    //if(name ==5)
+            //    //{
+            //    //    //break;
+            //    //    continue;
+            //    //}
+            //    Console.WriteLine(name);
+            //    if (name == 5)
+            //    {
+            //        name = 10;
+            //        //break;
+            //        //continue;
+            //    }
+            //}
+            #endregion for loop 
+
+            #region for loop  scenario
+            //for(int i=1; i<=5; i++)
+            // {
+            //     for(int j=1; j<=5; j++)
+            //     {
+            //         Console.Write("* \t ");
+            //     }
+            //   Console.WriteLine();
+            // }
+
+            //for(int i=1; i<=5; i++)
+            // {
+            //     for(int j=1; j<=i; j++)
+            //     {
+            //         Console.Write(" * ");
+            //     }
+            //     Console.WriteLine();
+            // }
+
+            ////Console.WriteLine("please enter a number");
+            ////int n=int.Parse(Console.ReadLine());
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(" * ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //Console.WriteLine("please enter a number");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(" * ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i=5; i>=1; i--)
+            //{
+            //    for (int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("* ");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion for loop  scenario
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
