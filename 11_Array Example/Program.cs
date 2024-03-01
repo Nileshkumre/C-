933365eb22bc56cf3fde6ddab34022cc727d5b6f
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-03-01 22:19:17 +0530

    11 - ArrayExample

---
+++ 11_Array Example/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Array_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array example divisible by 2
+            //Console.WriteLine("please enter a target number");
+            //int targets = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <=targets; i++)
+            //{
+            //    if(i%3 ==0)
+            //    {
+            //        Console.Write($"{i}  ");
+            //    }
+            //}
+            //Console.WriteLine();
+            //#endregion Array example divisible by 2
+            #endregion Array example divisible by 2
+
+            #region even number till given target
+
+            //Console.WriteLine("Please enter a target number");
+            //int target =int.Parse(Console.ReadLine());
+            //int evencount = target / 2;
+            //int[] evens =new int[evencount];
+            //int counter = 2;
+
+            //for (int i = 0; i < evencount; i++,counter+=2)
+            //{
+            //    evens[i] = counter;
+            //    //if(i%2 ==0)
+            //    //{
+            //    //    evens[i] = i;
+            //    //}
+
+            //}
+            //for (int i=0; i < evens.Length; i++)
+            //{
+            //    Console.Write($"{evens[i]} ");
+
+            //}
+            #endregion even number till given target
+
+            #region multidimetion array
+            //multi dimentional array
+            // array of arrays
+            ////int[] number = new int[2];
+            //int[,] number = new int[3, 3];
+            //for (int i = 0;i<3;i++)
+            //{
+            //    for(int j=0;j<3;j++)
+            //    {
+            //        Console.Write($"{i} {j} ");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            #endregion multidimetion array
+
+            #region multidimention array example
+            //int[] number = new int[2];
+            //int[,] number = new int[3, 3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        if(i==j)
+            //        {
+            //            Console.Write($"*\t ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("\t ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            #endregion multidimention array example
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
