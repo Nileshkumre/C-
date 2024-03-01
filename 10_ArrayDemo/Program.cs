938fa2e8577026cc213d500675654e8c53299b46
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-03-01 22:17:24 +0530

    10 - ArrayDemo

---
+++ 10_ArrayDemo/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ArrayDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declaring array = first way
+            //int[] fees = new int[3];
+            //fees[0] = 15000;
+            //fees[1] = 50000;
+            //fees[2] = 20000;
+
+            //Console.WriteLine($"Numbers of item ={fees.Length}");
+
+            //for(int i=0; i< fees.Length;i++)
+            //{
+            //    Console.Write($"{fees[i]} \n");
+            //}
+
+            //declaring array - second way
+
+            //string[] names = new string[] { "nilesh", "abhii", "Rupesh", "rahul" };
+            //Console.WriteLine($" Number of names : {names.Length}");
+
+            //for(int i =0; i< names.Length; i++)
+            //{
+            //    Console.Write($"\n {names[i]}");
+            //}
+
+            //string[] emails = null;
+            //////Console.WriteLine($"number of emails : {emails.Length}");
+            //string[] email = null;
+            //email = new string[] { "@gmial.com", "abhii@gmail.com","ajay@gmail.com" };
+            //Console.WriteLine($" lnumber of emails : {email.Length}");
+            //for (int i=0; i< email.Length; i++)
+            //{
+            //    Console.WriteLine($"{email[i ]}" );
+            //}
+
+            //int[] number = new int[5] { 4, 2, 5, 7,11 };
+            //Console.WriteLine($"lenght of number :{number.Length} numbers");
+            //for (int i=0; i<number.Length; i++)
+            //{
+            //    Console.Write($"{number[i]}" );
+            //}
+
+            //Console.WriteLine("\n Reverse array");
+            //for (int i = number.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write($"{number[i]}");
+            //}
+
+            /// find highest number
+            /// 
+
+            //int[] num = new int[5] { 4, 45, 34, 22, 44 };
+
+            //int max = num[0];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (num[i] > max)
+            //    {
+            //        max = num[i];
+            //    }   
+            //}
+            //Console.WriteLine(max);
+
+            
+            
+
+
+            
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
