965c3e35e0bb6b313af76d369c09cfeae5de26bc
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-03-04 10:43:20 +0530

    16 - BoxingUnboxing

---
+++ 16_BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine($"i : {i}");
+            object o = i;  // int --> object / value type --> reference type = boxing
+            Console.WriteLine($"o : {o}");
+            int j = (int)o;  // object --> int/ reference type --> value type = unboxing
+            Console.WriteLine($"j : {j}");
+            // object is base type for all types in dot net 
+            object o1 = 10;
+            o1 = "$";
+            o1 = "abc";
+            o1 = 45.5d;
+
+             
+
+
+            Console.ReadLine();
+        }
+    }
+}
