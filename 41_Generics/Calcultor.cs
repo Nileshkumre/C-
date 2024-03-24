252154cc198ed8ad31e1cd69974e10706baa3206
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-03-24 19:43:43 +0530

    41 - Generics

---
+++ 41_Generics/Calcultor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Generics
+{
+    internal class Calcultor
+    {
+        #region using method overloading 
+        //public bool AreEqual(int a, int b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        //public bool AreEqual(string a, string b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        //public bool AreEqual(float a, float b)
+        //{
+        //    return a.Equals(b);
+        //}
+        #endregion using method overloading 
+
+        #region using object type
+
+        //problems with object type
+        //1. there is no type safety
+        //2. chance of boxing & unboxing opretions
+
+        //public bool AreEqual (Object a, Object b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        #endregion using object type
+
+        #region using Generics
+
+        public bool AreEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+
+        public void Add<T>(T a, T b)
+        {
+            dynamic n1= a;
+            dynamic n2 = b;
+
+            Console.WriteLine(n1 + n2);
+        } 
+
+        #endregion using Generics
+
+
+    }
+}
---
+++ 41_Generics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcultor c1 = new Calcultor();
+            //bool b1 = c1.AreEqual(10, 10);
+            //Console.WriteLine(b1);
+
+            //b1 = c1.AreEqual(10, 2);
+            //Console.WriteLine(b1);
+
+            //b1 = c1.AreEqual("ABC","ABC");
+            //Console.WriteLine(b1);
+
+            //b1 = c1.AreEqual("10.5f", "2.5f");
+            //Console.WriteLine(b1);
+
+            //Calcultor c1 = new Calcultor();
+            //bool b1 =c1.AreEqual<int>(10, 10);
+            //Console.WriteLine(b1);
+
+            //b1 = c1.AreEqual<string>("Nilesh", "Nilesh");
+            //Console.WriteLine(b1);
+
+            //b1 = c1.AreEqual<float>(10.5f, 10.5f);
+            //Console.WriteLine(b1);
+
+            //c1.Add<int>(10, 12);
+            //c1.Add<string>("Nilesh", "kumre");
+            //c1.Add<float>(10.5f, 10.5f);
+
+            Student<int,string>s1 = new Student<int,string>();
+            s1.RollNumber = 1;
+            s1.Name = "Nilesh";
+            s1.Details();
+
+            Student<string,string>s2 = new Student<string,string>();
+            s2.RollNumber ="2" ;
+            s2.Name = "mahesh";
+            s2.Details();
+
+
+
+            Console.WriteLine(s1);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
