5ecf6dd091723cc393ac7d8dd69f2d379c66df2d
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-03-27 18:30:50 +0530

    46 - ConstReadOnlyKeyword

---
+++ 46_ConstReadOnlyKeyword/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_ConstReadOnlyKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1= new Student();
+            s1.RollNumber = 1;
+            s1.Name = "Nilesh";
+           // s1.CompanyName = "V#";
+            Console.WriteLine($"RollNumber : {s1.RollNumber} Name : {s1.Name} Conampny name : {s1.CompanyName}");
+            Student s2= new Student();
+            s2.RollNumber = 2;
+            s2.Name = "Mahi";
+            //s2.CompanyName = "V#";
+            
+            
+
+            Console.WriteLine($"RollNumber : {s2.RollNumber} Name : {s2.Name} Conampny name : {s2.CompanyName}");
+         
+
+            Console.ReadLine();
+        }
+    }
+    public class Student
+    {
+        public int RollNumber {  get; set; }    
+        public string Name { get; set; }
+        // public static string CompanyName = "V#";
+        //public const string CompanyName = "V#";
+        public  readonly string CompanyName;    
+        public Student()
+        {
+            CompanyName = "V#";
+        }
+    }
+}
