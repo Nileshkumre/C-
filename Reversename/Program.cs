399aed427564d063be9c4003afd864fe7c6defd1
Nileshkumre <161346350+Nileshkumre@users.noreply.github.com>
2024-04-04 22:31:41 +0530

    ReverseName

---
+++ Reversename/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7_Assingment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your name:");
+            string name=Console.ReadLine();
+            string reversename="";
+            for (int i= name.Length - 1; i>=0;i--)    
+            {
+                reversename+= name[i];
+            }
+            Console.WriteLine(reversename);
+            Console.ReadLine();
+        }
+    }
+}
